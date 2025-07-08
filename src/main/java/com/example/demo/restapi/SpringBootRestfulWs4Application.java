927e251f73f675de27ea4261a44289cb0f0a9ad4
Amaresh Reddy <amareshreddyamareshreddy491@gmail.com>
2025-07-08 12:07:41 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/restapi/SpringBootRestfulWs4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulWs4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulWs4Application.class, args);
+	}
+
+}
